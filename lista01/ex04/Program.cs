97dabec87aea54f7eb25c04b4c663b4a3bb46ea2
Wellinton Rafael Gudilunas <wellintongudilunas788@gmail.com>
2023-08-24 20:55:34 -0300

    chore: resolvido ex04 lista 01

---
+++ lista01/ex04/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ex04;
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("Digite a Idade: ");
+        int idade = int.Parse(Console.ReadLine());
+        
+        if(idade <= 13 && idade > 0) System.Console.WriteLine("Criança");
+        else if (idade > 13 && idade <= 18) System.Console.WriteLine("Adolescente");
+        else if (idade > 18 && idade <= 60) System.Console.WriteLine("Adulto");
+        else if (idade > 60) System.Console.WriteLine("Idoso");
+        else System.Console.WriteLine("Idade inválida");
+    }
+}
