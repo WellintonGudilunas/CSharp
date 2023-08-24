ce154481ea4eefcf0ac73d62db07729571a70520
Wellinton Rafael Gudilunas <wellintongudilunas788@gmail.com>
2023-08-24 20:55:07 -0300

    chore: resolvido ex03 lista 01

---
+++ lista01/ex03/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ex03;
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("Digite o primeiro número: ");
+        int n1 = int.Parse(Console.ReadLine());
+        System.Console.Write("Digite o segundo número: ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("O maior valor é: " + (n1 > n2 ? n1 : n2));
+    }
+}
