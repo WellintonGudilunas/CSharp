a46700d7fdec7515848e24e8e432cba435314551
Wellinton Rafael Gudilunas <wellintongudilunas788@gmail.com>
2023-08-24 20:54:41 -0300

    chore: resolvido ex01 lista 01

---
+++ lista01/ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex01;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a altura do retângulo: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a largura do retângulo: ");
+        double largura = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("A área do retangulo é: " + CalculaAreaRetangulo(altura, largura));
+    }
+
+    public static double CalculaAreaRetangulo(double altura, double largura)
+    {
+        return altura * largura;
+    }
+}
